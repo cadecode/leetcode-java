9dfbe12c7e5d8467f94bc65da9203e256bb29235
Cade Li <cadecode@foxmail.com>
2021-08-07 21:17:43 +0800

    feat: 添加【3. 无重复字符的最长子串】

---
+++ src/main/java/cade/struc/hash/P2LengthOfLongestSubstringSolution.java
@@ -0,0 +1,30 @@
+package cade.struc.hash;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class P2LengthOfLongestSubstringSolution {
+    public int lengthOfLongestSubstring(String s) {
+        int len = s.length();
+        // 处理特殊情况：空串和一个字符
+        if (len <= 1) return len;
+        Map<Character, Integer> cache = new HashMap<>();
+        int max = 0, left = 0, right = 0;
+        while (right < len) {
+            char c = s.charAt(right);
+            // 判断是否缓存过该字符，其该字符在当前窗口内
+            if (cache.containsKey(c) && cache.get(c) >= left) {
+                // 向左滑动窗口
+                left = cache.get(c) + 1;
+            }
+            // 缓存字符和索引
+            cache.put(c, right);
+            // 比较大小，此时保证索引 left 到 right 不重复
+            max = Math.max(max, right - left + 1);
+            right++;
+        }
+        return max;
+    }
+}
