b87ec70233bcea9cb19a847975db8915aa395ca7
Cade Li <cadecode@foxmail.com>
2021-08-07 12:00:53 +0800

    refactor: 修改 Test 目录，添加 logback、gitignore 文件

--- src/test/java/cade/AppTest.java
+++ src/test/java/cade/AppTest.java
@@ -1,10 +1,12 @@
 package cade;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AppTest {
+
+    public final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Test
     public void test() {
