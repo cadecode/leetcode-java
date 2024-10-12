30683f552d77289c9006143dda9d4ec1d91e20b5
Cade Li <cade.li@qq.com>
2024-10-12 17:14:02 +0800

    fix: 修改排序算法不规范逻辑

--- src/main/java/top/cadecode/learn/leetcode/demo/sort/BubbleSort.java
+++ src/main/java/top/cadecode/learn/leetcode/demo/sort/BubbleSort.java
@@ -10,11 +10,10 @@ public class BubbleSort {
     public static void sort(int[] arr) {
         if (arr == null || arr.length < 2) return;
 
-        // 从每一位开始，向后两两比较
         for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] > arr[j]) {
-                    SortTool.swap(arr, i, j);
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    SortTool.swap(arr, j, j - 1);
                 }
             }
         }
--- src/main/java/top/cadecode/learn/leetcode/demo/sort/InsertSort.java
+++ src/main/java/top/cadecode/learn/leetcode/demo/sort/InsertSort.java
@@ -10,11 +10,13 @@ public class InsertSort {
     public static void sort(int[] arr) {
         if (arr == null || arr.length < 2) return;
 
-        // 每一位和前面的元素比较，直到找到自己的位置
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i + 1; j > 0; j--) {
-                if (arr[j] >= arr[j - 1]) break;
-                SortTool.swap(arr, j, j - 1);
+                if (arr[j] < arr[j - 1]) {
+                    SortTool.swap(arr, j, j - 1);
+                } else {
+                    break;
+                }
             }
         }
     }
